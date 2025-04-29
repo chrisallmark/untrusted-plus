20c02ebef595a2a5286ab4a04405eadf97db42ee
Chris Allmark <chris.allmark@gmail.com>
2025-04-29 18:13:44 +0100

    code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
-
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
-  router.push("/untrusted/index.html");
+  useEffect(() => {
+    router.push("/untrusted/index.html");
+  }, [router]);
 }
